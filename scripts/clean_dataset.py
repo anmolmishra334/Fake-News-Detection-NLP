21865fd10d7389e404f35d0073f08e7b7459238e
anmolmishra334 <147097705+anmolmishra334@users.noreply.github.com>
2025-06-04 15:15:47 +0530

    Update clean_dataset.py

--- scripts/clean_dataset.py
+++ scripts/clean_dataset.py
@@ -38,5 +38,5 @@ def clean_data(df):
       print("Falied to clean dataset, issue with csv file entered, please check and fix before uploading.")
   else:
       print("Dataset cleaned successfully.")
-df=pd.read_csv('fake_news_final')
+df=pd.read_csv('dataset/fake_news_final.csv')
 clean_data(df)
