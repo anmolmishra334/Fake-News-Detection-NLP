b2b565e176b385e71f3bbe711ca42c80120c0564
anmolmishra334 <147097705+anmolmishra334@users.noreply.github.com>
2025-06-19 00:54:27 +0530

    Update train_model.py

--- scripts/train_model.py
+++ scripts/train_model.py
@@ -1,4 +1,4 @@
-_import pandas as pd
+import pandas as pd
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.linear_model import LogisticRegression
